b5f18fc34a506d618e713b87c42f57b845f40b9a
Anton Miroshnichenko <a.mir.inout@gmail.com>
2020-10-28 00:20:45 +0200

    Removed unneeded lines

--- src/main.cpp
+++ src/main.cpp
@@ -21,37 +21,4 @@ void loop()
   control_panel.check_switch();
   toilet.check_enter_pin();
   toilet.check_sleep();
-  
-  // if (led_strip_on && !sleep_mode) actual_brightness = map(analogRead(KRUTILKA), 0, 1023, 0, 255);
-  
-  
-  // if (sleep_timer.onExpired()) 
-  // {
-  //   tone(SOUND_PIN, NOTE_D2, 300);
-  //   delay(300);
-  //   noTone(SOUND_PIN);
-  //   blue_led_blink_timer.restart();
-  //   for (int i=actual_brightness; i==0; i--)
-  //   {
-  //     FastLED.setBrightness(i);
-  //     FastLED.show();
-  //     FastLED.delay(1000/UPDATES_PER_SECOND);
-  //   }
-  //   sleep_mode = true;
-  // }
-
-  // if (blue_led_blink_timer.onExpired())
-  // {
-  //   if (blue_led_state)
-  //   {
-  //     Tualet::set_enter_led(BLUE);
-  //   }
-  //   else
-  //   {
-  //     digitalWrite(LED_BLUE,HIGH);
-  //     digitalWrite(LED_RED,HIGH);
-  //     digitalWrite(LED_GREEN,HIGH);       
-  //   }
-  //   blue_led_blink_timer.restart();
-  // }
 }   
